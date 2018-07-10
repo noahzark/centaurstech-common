986742fcf8f258deeee38c238880f9be0707f8a9
Feliciano Long <noahzark@vip.qq.com>
2018-07-10 02:17:08 -0500

    optimize exception handler in md5

--- src/main/java/com/centaurstech/utils/Md5.java
+++ src/main/java/com/centaurstech/utils/Md5.java
@@ -6,18 +6,24 @@ import java.security.NoSuchAlgorithmException;
 
 public final class Md5 {
 
-	public static String digest(String plaintext) throws NoSuchAlgorithmException {
-		MessageDigest m = MessageDigest.getInstance("MD5");
-		m.reset();
-		m.update(plaintext.getBytes());
+	public static String digest(String plaintext) {
+		String hashtext = null;
+		try {
+			MessageDigest m = MessageDigest.getInstance("MD5");
 
-		byte[] digest = m.digest();
-		BigInteger bigInt = new BigInteger(1, digest);
+			m.reset();
+			m.update(plaintext.getBytes());
 
-		String hashtext = bigInt.toString(16);
-		// Now we need to zero pad it if you actually want the full 32 chars.
-		while( hashtext.length() < 32 ){
-			hashtext = "0" + hashtext;
+			byte[] digest = m.digest();
+			BigInteger bigInt = new BigInteger(1, digest);
+
+			hashtext = bigInt.toString(16);
+			// Now we need to zero pad it if you actually want the full 32 chars.
+			while( hashtext.length() < 32 ){
+				hashtext = "0" + hashtext;
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
 		}
 
 		return hashtext;
