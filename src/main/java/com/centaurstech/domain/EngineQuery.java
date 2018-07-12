13afed423bce9a121233ceaafe36868ea38a0a29
Feliciano Long <noahzark@vip.qq.com>
2018-07-12 00:19:37 -0500

    add has value, get string value static methods to engine query

--- src/main/java/com/centaurstech/domain/EngineQuery.java
+++ src/main/java/com/centaurstech/domain/EngineQuery.java
@@ -51,4 +51,34 @@ public abstract class EngineQuery {
     public String getQueryTimeString() {
         return "Query takes " + getQueryTime() + "ms";
     }
+
+    /**
+     * Check if engine form has a valuable key
+     * @param requestParams
+     * @param key
+     * @return
+     */
+    static Boolean hasValue(Map<String,String> requestParams, String key) {
+        String result = requestParams.getOrDefault(key, null);
+        if (result != null && !result.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get a value using a series of keys from engine form
+     * @param requestParams
+     * @param keys
+     * @return
+     */
+    static String getStringValue(Map<String, String> requestParams, String... keys) {
+        for (String key : keys) {
+            if (hasValue(requestParams, key)) {
+                return requestParams.get(key);
+            }
+        }
+        return "";
+    }
+
 }
