d9ceced7d7a204c4bf6fdfe0ac97fa1285b8cba1
Feliciano Long <noahzark@vip.qq.com>
2018-07-11 23:42:42 -0500

    add gps location entity and distance calculator

---
+++ src/main/java/com/centaurstech/domain/GPSLocation.java
@@ -0,0 +1,31 @@
+package com.centaurstech.domain;
+
+/**
+ * Created by Feliciano on 7/11/2018.
+ */
+public class GPSLocation {
+
+    Double lat;
+    Double lng;
+
+    public GPSLocation(Double lat, Double lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+}
---
+++ src/main/java/com/centaurstech/utils/DistanceCalculator.java
@@ -0,0 +1,36 @@
+package com.centaurstech.utils;
+
+import com.centaurstech.domain.GPSLocation;
+
+/**
+ * Created by Feliciano on 7/11/2018.
+ */
+public class DistanceCalculator {
+
+    static int EARTH_RADIUS_IN_KM = 6371;
+
+    private static double deg2rad(double deg){
+        return deg*(Math.PI/180);
+    }
+
+    double distanceBetween(GPSLocation source, GPSLocation target) {
+        return distanceBetween(source.getLat(), source.getLng(), target.getLat(), target.getLng());
+    }
+
+    double distanceBetween(double sourceLat, double sourceLng, double targetLat, double targetLng) {
+        return distanceBetween(sourceLat, sourceLng, targetLat, targetLng, EARTH_RADIUS_IN_KM);
+    }
+
+    double distanceBetween(double sourceLat, double sourceLng, double targetLat, double targetLng, int radius) {
+        double dLat = deg2rad(targetLat - sourceLat);
+        double dLon = deg2rad(targetLng - sourceLng);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(deg2rad(sourceLat)) * Math.cos(deg2rad(targetLat)) *
+                        Math.sin(dLon/2) * Math.sin(dLon/2);
+        double c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        double d = radius * c;
+
+        return d;
+    }
+
+}
