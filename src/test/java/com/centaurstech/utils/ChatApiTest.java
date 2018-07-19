fcc5878b672efe6c4a0e2fcfef0da42f7e44782b
Feliciano Long <noahzark@vip.qq.com>
2018-07-18 20:03:45 -0600

    optimize import and add query time count for chat api test

--- src/test/java/com/centaurstech/utils/ChatApiTest.java
+++ src/test/java/com/centaurstech/utils/ChatApiTest.java
@@ -1,12 +1,13 @@
 package com.centaurstech.utils;
 
+import com.centaurstech.domain.EngineQuery;
+import com.centaurstech.domain.EngineQueryProxy;
 import com.centaurstech.domain.GPSLocation;
 import org.hamcrest.core.IsNull;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,6 +19,8 @@ public class ChatApiTest {
 
     @Test
     public void testChatApi() throws Exception {
+        EngineQuery engineQuery = new EngineQueryProxy("/api/chat");
+
         String appkey = "qiwurobot";
         String appsecret = "123456";
         String nickname = "common-lib-test";
@@ -28,10 +31,14 @@ public class ChatApiTest {
         JSONObject result = chatApi.chat(appkey, appsecret, uid, nickname,
                 "HELLO");
         assertThat(result.get("retcode"), is(0));
+
+        System.out.println(engineQuery.getQueryTimeString());
     }
 
     @Test
     public void testSendJson() throws Exception {
+        EngineQuery engineQuery = new EngineQueryProxy("/api/chat/data");
+
         ChatApi chatApi = new ChatApi("https://robot-service.centaurstech.com/api/chat/data");
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("hello", "world");
@@ -39,20 +46,28 @@ public class ChatApiTest {
         String SERVER_SALT = " QUERY_RESULT_SALT";
         String ticket = chatApi.sendJson("test_result_type", jsonObject, SERVER_SALT);
         assertThat(ticket, is(IsNull.notNullValue()));
+
+        System.out.println(engineQuery.getQueryTimeString());
     }
 
     @Test
     public void testSendGPS() throws Exception {
+        EngineQuery engineQuery = new EngineQueryProxy("/api/chat/geo");
+
         GPSLocation location = new GPSLocation(1d, 2d);
 
         ChatApi chatApi = new ChatApi("https://robot-service.centaurstech.com/api/chat/geo");
 
         String msg = chatApi.sendGPS("qiwurobot", "123456", "common-lib-test", location);
         assertThat(msg, is("added"));
+
+        System.out.println(engineQuery.getQueryTimeString());
     }
 
     @Test
     public void testEngineLogin() throws Exception {
+        EngineQuery engineQuery = new EngineQueryProxy("/goingchatcn");
+
         ChatApi chatApi = new ChatApi("http://qcloud-sh1.chewrobot.com/goingchatcn/chatbotserver.php");
         HashMap<String, String> loginRequest = new HashMap<>();
         loginRequest.put("aipioneer_username", "mimi2");
@@ -61,6 +76,8 @@ public class ChatApiTest {
 
         assertThat(result, is(IsNull.notNullValue()));
         assertThat(result.has("chat_key"), is(true));
+
+        System.out.println(engineQuery.getQueryTimeString());
     }
 
 }
