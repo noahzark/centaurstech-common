481e983bcd43973e9ec0e23274f103a613f3897f
Feliciano Long <noahzark@vip.qq.com>
2018-08-09 15:45:38 -0500

    add query helper and its test

---
+++ src/main/java/com/centaurstech/utils/QueryHelper.java
@@ -0,0 +1,31 @@
+package com.centaurstech.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+public class QueryHelper {
+
+    public static String urlEncodeUTF8(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UnsupportedOperationException(e);
+        }
+    }
+
+    public static String urlEncodeUTF8(Map<?,?> map) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<?,?> entry : map.entrySet()) {
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(String.format("%s=%s",
+                    urlEncodeUTF8(entry.getKey().toString()),
+                    urlEncodeUTF8(entry.getValue().toString())
+            ));
+        }
+        return sb.toString();
+    }
+
+}
---
+++ src/test/java/com/centaurstech/utils/QueryHelperTest.java
@@ -0,0 +1,25 @@
+package com.centaurstech.utils;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class QueryHelperTest {
+
+
+    @Test
+    public void testEncodeURL() {
+        HashMap<String, String> query = new HashMap<>();
+        query.put("a", "1");
+        query.put("b", "2");
+        query.put("c", "3");
+
+        String queryString = QueryHelper.urlEncodeUTF8(query);
+        System.out.println(queryString);
+        assertThat(queryString, is("a=1&b=2&c=3"));
+    }
+
+}
