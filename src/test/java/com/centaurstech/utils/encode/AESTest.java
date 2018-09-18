585dd2ae528c54f3f45bd99abc7fd2ead1b22ceb
Feliciano Long <noahzark@vip.qq.com>
2018-09-17 22:27:48 -0500

    add aes cipher and its test

---
+++ src/test/java/com/centaurstech/utils/encode/AESTest.java
@@ -0,0 +1,115 @@
+package com.centaurstech.utils.encode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.AlgorithmParameters;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.util.UUID;
+
+public class AESTest {
+
+    public static String SecretKey2String(SecretKey secretKey) {
+        return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+    }
+
+    public static SecretKey String2SecretKey(String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+    }
+
+    @Test
+    public void testAESCipher() throws Exception {
+        String encryptResult = AESCipher.aesEncryptString(
+                "abcdefghijklmnopqRSTUVWXYZ1234567890!@#$%^&*()_+~",
+                "ZGRrMgGs/D9M8UbrOV+BMg==",
+                "0bdN6hsVC5vVPVNUmMnFRg=="
+        );
+        System.out.println(encryptResult);
+
+        String decyryptResult = AESCipher.aesDecryptString(
+                "8jI+xr/TlLwzM0+vssSGm55ySTZX3nUTgZIkfS6XQjBL5UXQPa3WBEU6lq0Njm+QfJuXZvarq4/3MkO49gpRYQ==",
+                "ZGRrMgGs/D9M8UbrOV+BMg==",
+                "0bdN6hsVC5vVPVNUmMnFRg==");
+        System.out.println(decyryptResult);
+    }
+
+    @Test
+    public void test128EncodeAndDecode() throws Exception {
+        String test = "abcdefghijklmnopqRSTUVWXYZ1234567890!@#$%^&*()_+~";
+        String password = UUID.randomUUID().toString().replaceAll("-", "").substring(16);
+
+        SecureRandom rand = new SecureRandom();
+        byte[] salt = new byte[8];
+        rand.nextBytes(salt);
+
+        /* Derive the key, given password and salt. */
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+        System.out.println("secret: " + SecretKey2String(secret));
+
+        /* Encrypt the message. */
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        AlgorithmParameters params = cipher.getParameters();
+        byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+        byte[] ciphertext = cipher.doFinal(test.getBytes("UTF-8"));
+
+        String cipherString = new String();
+        System.out.println("iv: " + new String(Base64.getEncoder().encodeToString(iv)));
+        System.out.println("ciphertext: " + new String(Base64.getEncoder().encode(ciphertext)));
+
+        /* Decrypt the message, given derived key and initialization vector. */
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+        String plaintext = new String(cipher.doFinal(ciphertext), "UTF-8");
+        System.out.println(plaintext);
+    }
+
+    @Test
+    public void test256EncodeAndDecode() throws Exception {
+        Security.setProperty("crypto.policy", "unlimited");
+
+        String test = "abcdefghijklmnopqRSTUVWXYZ1234567890!@#$%^&*()_+~";
+        String password = UUID.randomUUID().toString().replaceAll("-", "");
+
+        SecureRandom rand = new SecureRandom();
+        byte[] salt = new byte[8];
+        rand.nextBytes(salt);
+
+        /* Derive the key, given password and salt. */
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+        /* Encrypt the message. */
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        AlgorithmParameters params = cipher.getParameters();
+        byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+        byte[] ciphertext = cipher.doFinal(test.getBytes("UTF-8"));
+
+        String cipherString = new String(Base64.getEncoder().encode(ciphertext));
+        System.out.println(cipherString);
+
+        /* Decrypt the message, given derived key and initialization vector. */
+        cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(iv));
+        String plaintext = new String(cipher.doFinal(ciphertext), "UTF-8");
+        System.out.println(plaintext);
+    }
+
+}
