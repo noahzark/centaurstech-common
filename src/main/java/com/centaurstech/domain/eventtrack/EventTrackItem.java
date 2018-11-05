b8b56cc77b95abfc9aff16ddbd03bf0f6d546d6f
Fangzhou Long <feliciano.long@gmail.com>
2018-11-05 22:24:24 +0800

    add event track item classes

---
+++ src/main/java/com/centaurstech/domain/eventtrack/EventTrackItem.java
@@ -0,0 +1,185 @@
+package com.centaurstech.domain.eventtrack;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Event track items
+ *
+ * @author Tongcheng.Tang
+ */
+public class EventTrackItem {
+    private String reportType;
+    private String timestamp;
+    private Map<String, String> fields;
+
+    public EventTrackItem(String reportType, Map<String, String> fields) {
+        this.reportType = reportType;
+        this.timestamp = new Date().getTime() + "";
+        this.fields = fields;
+    }
+
+    public EventTrackItem(String reportType) {
+        this.reportType = reportType;
+        this.timestamp = new Date().getTime() + "";
+        this.fields = new HashMap<>();
+    }
+
+    public EventTrackItem() {
+        this.fields = new HashMap<>();
+    }
+
+    public String getReportType() {
+        return reportType;
+    }
+
+    public void setReportType(String reportType) {
+        this.reportType = reportType;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Map<String, String> getFields() {
+        return fields;
+    }
+
+    public void setFields(Map<String, String> fields) {
+        this.fields = fields;
+    }
+
+    public void putAttribute(String key, String value) {
+        this.fields.put(key, value);
+    }
+
+    public enum ReportType {
+        USER_BEHAVIOR("上报⽤户⾏为数据"),
+        BOT_ACTIVATION("上报BOT唤醒数据"),
+        BOT_EXCEPTION("上报BOT异常数据"),
+        BOT_SESSION("上报BOT会话数据");
+        private String description;
+
+        ReportType(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum Platform {
+        SMART_LIFE_APP("智慧生活App"),
+        XIAO_MI("小爱音箱");
+        private String description;
+
+        Platform(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum ActionType {
+        INSTALL("⽤户安装\"你好⼩悟\""),
+        STARTUP("用户启动\"你好小悟\""),
+        REGISTER("用户注册"), LOGIN("用户登录"),
+        UPVOTE("点赞"),
+        DOWNVOTE("点踩"),
+        IMPOWER("用户授权");
+        private String description;
+
+        ActionType(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum PermissionType {
+        LOCATION("位置权限"),
+        MICROPHONE("⻨克⻛权限"),
+        CONTACTS("通讯录权限"),
+        PHONE("通话权限"),
+        CAMERA("相机权限"),
+        GALLERY("相册权限");
+        private String description;
+
+        PermissionType(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum Field {
+        NAVIGATION_ARRIVED("导航到达⽬的地"),
+        USE_NAVIGATION_FOR_BIKING("使⽤导航骑⾏"),
+        USE_NAVIGATION_FOR_DRIVING("使⽤导航驾驶"),
+        USE_NAVIGATION_FOR_WALK("使⽤导航步⾏"),
+        USE_NAVIGATION_FOR_PUB_TRANSPORT("使⽤导航乘坐公共交通⼯具"),
+        ORDER_QUERY("订单请求"),
+        ORDER_RECOMMEND("订单推荐"),
+        ORDER_RECOMMEND_FIRST("⾸次订单推荐"),
+        ORDER_COMMIT("订单提交"),
+        ORDER_CHECKOUT("订单成交"),
+        ORDER_TICKET("订单出票"),
+        ORDER_REFUND("订单退款"),//该行需要bot
+        SESSION_FINISHED("对话完成"),
+        FLIGHT_CHANGE_SPACE("机票换舱位"),
+        FLIGHT_CHANGE_TIME("机票换时间"),
+        FLIGHT_CHANGE_PRICE("机票换价格"),
+        FLIGHT_CHANGE_AIRLINE("机票换航空公司"),
+        TRAIN_CHANGE_SEAT("⽕⻋票换座位"),
+        TRAIN_CHANGE_TIME("⽕⻋票换时间"),
+        TRAIN_CHANGE_PRICE("⽕⻋票换价格"),
+        TRAIN_CHANGE_MODEL("⽕⻋票换⻋类型"),
+        HOTEL_CHANGE_STAR("酒店换星级"),
+        HOTEL_CHANGE_DATE("酒店换⽇期"),
+        HOTEL_CHANGE_PRICE("酒店换价格"),
+        HOTEL_CHANGE_MODEL("酒店换房型"),
+        WEATHER_QUERY_DATE("天⽓查询⽇期"),
+        WEATHER_QUERY_SITE("天⽓查询地点"),
+        WEATHER_MULTI_DAY("天⽓查询多天"),
+        NAVIGATION_QUERY_ROUND("导航查询周边"),
+        NAVIGATION_QUERY_SITE("导航查询地点"),
+        NAVIGATION_START("开始导航"),
+        NAVIGATION_END("结束导航"),
+        NAVIGATION_SWITCH("导航切换");
+        private String description;
+
+        Field(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum BotName {
+        TRAIN("火车"),
+        FLIGHT("机票"),
+        HOTEL("酒店"),
+        NAVIGATION("导航"),
+        WEATHER("天气"),
+        ALLTAXIS("打车"),
+        FREECHAT("闲聊"),
+        UNKNOWN("未知");
+        private String description;
+
+        BotName(String description) {
+            this.description = description;
+        }
+    }
+
+    public enum FieldKey {
+        ACTION("action"),
+        PERMISSION("permission"),
+        BOT("bot"),
+        EXCEPTION("exception"),
+        FIELD("field"),
+        CHAT("chat");
+        public String value;
+
+        FieldKey(String value) {
+            this.value = value;
+        }
+    }
+
+    public interface BotException {
+        String getExceptionDescription();
+    }
+}
---
+++ src/main/java/com/centaurstech/domain/eventtrack/EventTrackProxy.java
@@ -0,0 +1,125 @@
+package com.centaurstech.domain.eventtrack;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Event track proxy
+ *
+ * @author Tongcheng.Tang
+ * updated by Fangzhou.Long
+ */
+public class EventTrackProxy {
+
+    private static final int MAX_SIZE_STORAGE = 50;
+    String origin;
+    EventTrackSender eventTrackSender;
+    private HashSet<EventTrack> eventTrackSet = new HashSet<>(MAX_SIZE_STORAGE * 2);
+    private ExecutorService cachePool = Executors.newCachedThreadPool();
+
+    public EventTrackProxy(EventTrackSender eventTrackSender, String origin) {
+        this.eventTrackSender = eventTrackSender;
+        this.origin = origin;
+    }
+
+    public String addUserBehaviorEvent(String uid, EventTrackItem.ActionType actionType, EventTrackItem.PermissionType permissionType) {
+        Map<String, String> fields = new HashMap<>();
+        fields.put(EventTrackItem.FieldKey.ACTION.value, actionType.toString());
+        if (actionType == EventTrackItem.ActionType.IMPOWER) {
+            if (permissionType == null) {
+                return "permission missing";
+            }
+        }
+        fields.put(EventTrackItem.FieldKey.PERMISSION.value, permissionType.toString());
+        EventTrack eventTrack = generateEventTrack(uid, EventTrackItem.ReportType.USER_BEHAVIOR, fields);
+        eventTrack.setOrigin(origin);
+        this.submitTask(eventTrack);
+        return "SUCCESS";
+    }
+
+    public String addBotActivationEvent(String uid, EventTrackItem.Platform platform, EventTrackItem.BotName botName) {
+        Map<String, String> fields = new HashMap<>();
+        if (botName != null) {
+            fields.put(EventTrackItem.FieldKey.BOT.value, botName.toString());
+        }
+        EventTrack eventTrack = generateEventTrack(uid, EventTrackItem.ReportType.BOT_ACTIVATION, fields);
+        eventTrack.setPlatform(platform == null ? null : platform.toString());
+        eventTrack.setOrigin(origin);
+        this.submitTask(eventTrack);
+        return "SUCCESS";
+    }
+
+    public String addBotExceptionEvent(String uid, EventTrackItem.Platform platform, EventTrackItem.BotException exceptionType) {
+        Map<String, String> fields = new HashMap<>();
+        if (exceptionType == null) {
+            return "exception missing";
+        } else {
+            fields.put(EventTrackItem.FieldKey.EXCEPTION.value, exceptionType.getExceptionDescription());
+        }
+        EventTrack eventTrack = generateEventTrack(uid, EventTrackItem.ReportType.BOT_EXCEPTION, fields);
+        eventTrack.setPlatform(platform == null ? null : platform.toString());
+        eventTrack.setOrigin(origin);
+        this.submitTask(eventTrack);
+        return "SUCCESS";
+    }
+
+    public String addBotSessionEvent(String uid, EventTrackItem.Platform platform, EventTrackItem.Field field, EventTrackItem.BotName botName, Integer chat) {
+        Map<String, String> fields = new HashMap<>();
+        fields.put(EventTrackItem.FieldKey.FIELD.value, field.toString());
+        if (field == EventTrackItem.Field.ORDER_QUERY || field == EventTrackItem.Field.ORDER_RECOMMEND ||
+                field == EventTrackItem.Field.ORDER_RECOMMEND_FIRST || field == EventTrackItem.Field.ORDER_COMMIT ||
+                field == EventTrackItem.Field.ORDER_CHECKOUT || field == EventTrackItem.Field.ORDER_REFUND ||
+                field == EventTrackItem.Field.ORDER_TICKET) {
+            //必须带bot name
+            if (botName == null) {
+                return "bot name missing";
+            } else {
+                fields.put(EventTrackItem.FieldKey.BOT.value, botName.toString());
+            }
+        }
+        if (field == EventTrackItem.Field.SESSION_FINISHED) {
+            //必须带chat number
+            if (chat == null) {
+                return "chat number missing";
+            } else {
+                fields.put(EventTrackItem.FieldKey.CHAT.value, chat.toString());
+            }
+        }
+        EventTrack eventTrack = generateEventTrack(uid, EventTrackItem.ReportType.BOT_SESSION, fields);
+        eventTrack.setPlatform(platform == null ? null : platform.toString());
+        eventTrack.setOrigin(origin);
+        this.submitTask(eventTrack);
+        return "SUCCESS";
+    }
+
+    private EventTrack generateEventTrack(String uid, EventTrackItem.ReportType reportType, Map<String, String> fields) {
+        EventTrack eventTrack = new EventTrack();
+        eventTrack.setUid(uid);
+        EventTrackItem eventTrackItem = new EventTrackItem(reportType.toString());
+        eventTrackItem.setFields(fields);
+        eventTrack.addDataItem(eventTrackItem);
+        return eventTrack;
+    }
+
+    private void submitTask(EventTrack eventTrack) {
+        Runnable runnable = () -> checkTrackSet(eventTrack);
+        cachePool.execute(runnable);
+    }
+
+    private synchronized void checkTrackSet(EventTrack eventTrack) {
+        eventTrackSet.add(eventTrack);
+        if (eventTrackSet.size() >= MAX_SIZE_STORAGE) {
+            eventTrackSender.sendEventsToServer(eventTrackSet);
+            eventTrackSet.clear();
+        }
+    }
+
+    public interface EventTrackSender {
+        void sendEventsToServer(Set<EventTrack> eventTracks);
+    }
+
+}
